43590abe6be6b1e499d1edc758dd08dbc992287c
Almann Goo <almann@amazon.com>
2022-08-05 12:09:01 -0700

    Adds more scalar types

--- partiql-eval/src/data.rs
+++ partiql-eval/src/data.rs
@@ -5,8 +5,12 @@
 //! This module can be thought of as providing the moral equivalent of *data frames* for PartiQL.
 //! We refrain from using that terminology here because data frames typically refer to tables with
 //! rows that have columns with fully concrete schema types.
+//!
+//! This API is currently experimental and under heavy development.
 
+use bigdecimal::BigDecimal;
 use num_bigint::BigInt;
+use rust_decimal::Decimal;
 
 /// Representation of `NULL` and `MISSING`.
 pub enum NullKind {
@@ -15,21 +19,30 @@ pub enum NullKind {
 }
 
 /// Various representation of integers.
-pub enum Int {
-    // TODO determine if we need the unsigned ones
+pub enum IntKind {
     I32(i32),
     I64(i64),
     Big(BigInt),
 }
 
 /// 32/64-bit float.
-pub enum Float {
+pub enum FloatKind {
     F32(f32),
     F64(f64),
 }
 
+/// Decimal
+pub enum DecimalKind {
+    Fixed(Decimal),
+    Arbitrary(BigDecimal),
+}
+
+// TODO the timestamp/time/date/etc.
+
+/// Represents a scalar value
 pub enum Scalar {
     Null(NullKind),
     Bool(bool),
-    Int,
+    Int(IntKind),
+    Decimal(DecimalKind),
 }
--- partiql-eval/src/lib.rs
+++ partiql-eval/src/lib.rs
@@ -3,11 +3,26 @@ pub mod data;
 /// Re-exports for our public API.
 pub mod ext {
     pub use bigdecimal;
-    pub use num_integer;
+    pub use num_bigint;
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::data::*;
+    use bigdecimal::BigDecimal;
+    use num_bigint::BigInt;
+    use rust_decimal::Decimal;
+    use std::mem::size_of;
+
     #[test]
-    fn todo() {}
+    fn todo() {
+        let bigint_size = size_of::<BigInt>();
+        println!("BigInt: {bigint_size}");
+        let bigdec_size = size_of::<BigDecimal>();
+        println!("BigDec: {bigdec_size}");
+        let dec_size = size_of::<Decimal>();
+        println!("Decimal: {dec_size}");
+        let scalar_size = size_of::<Scalar>();
+        println!("Scalar: {scalar_size}");
+    }
 }
